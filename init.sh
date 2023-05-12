f92806d36f927aad4e05a20abaf405e6feb35890
Jade <1001227+JadeQin@users.noreply.github.com>
2023-05-12 16:07:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 # install docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt update
+sudo apt install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -8,6 +8,11 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt update
+sudo apt install -y docker-ce
 
+# install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# install xrdp
+sudo apt install -y xrdp
